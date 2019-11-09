4fdcbd11a7804cc0313cac672874b94a20842368
ganttArt <gantt.art@gmail.com>
2019-11-08 16:06:17 -0800

    web server iterations including led, switch, and light level readings

--- web_server/main.py
+++ web_server/main.py
@@ -47,11 +47,42 @@ def dummy():
 
     return response_template % body
 
-pin = machine.Pin(10, machine.Pin.IN)
+# # using the server to turn a light on and off
+# pin = machine.Pin(9, machine.Pin.OUT)
+
+# def light_on():
+#     pin.value(1)
+#     body = "You turned a light on!"
+#     return response_template % body
+
+# def light_off():
+#     pin.value(0)
+#     body = "You turned a light off!"
+#     return response_template % body
+
+# # using the server to see if a switch is pushed or not
+# switch_pin = machine.Pin(10, machine.Pin.IN)
+
+# def switch():
+#     body = "{state: " + str(switch_pin.value()) + "}"
+#     return response_template % body
+
+
+# using the server to see the brightness level of the room
+adc = machine.ADC(0)
+
+def light():
+    body = "{value: " + str(adc.read()) + "}"
+    return response_template % body
+
 
 handlers = {
     'time': time,
     'dummy': dummy,
+    # 'light_on': light_on,
+    # 'light_off': light_off,
+    # 'switch': switch,
+    'light': light,
 }
 
 def main():
